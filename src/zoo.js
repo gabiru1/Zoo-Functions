b63a746a8a7becabe4b74bf55be8de90a719c776
Gabriel Sampaio Bicalho <gabrielsampaiobicalho@gmail.com>
2022-02-08 14:52:01 -0300

    post in mt repo

--- src/zoo.js
+++ src/zoo.js
@@ -1,40 +1,40 @@
 const data = require('./data');
-
+// desestruturação do data.js
 const { species, employees, hours, prices } = data;
 
-function getSpeciesByIds(...ids) {
+function getSpeciesByIds(...ids) { // Permite representar um numero indefinido de argumentos
   // seu código aqui
-  return species.filter((animals, i) => animals.id === ids[i]);
+  return species.filter((animals, i) => animals.id === ids[i]); // retorna nenhum, um ou varios animais referentes ao id do parametro dentro de um array
 }
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
-  const selectAnimal = species.find((obj) => obj.name === animal);
-  return selectAnimal.residents.every((pet) => pet.age >= age);
+  const selectAnimal = species.find((obj) => obj.name === animal); // armazena o objeto do id informado em uma constante
+  return selectAnimal.residents.every((pet) => pet.age >= age); // true se todos sao, false se ao menos 1 nao eh
 }
 
 function getEmployeeByName(e) {
   // seu código aqui
   if (arguments.length === 0) return {};
-  return employees.find((info) => info.firstName === e || info.lastName === e);
+  return employees.find((info) => info.firstName === e || info.lastName === e); // retorna o funcionario cujo firstName || lastName batem com o argumento do parametro
 }
 
 function createEmployee(personalInfo, associatedWith) {
   // seu código aqui
   return {
-    ...personalInfo,
+    ...personalInfo, // espalha os argumentos dentro do objeto usando spread
     ...associatedWith,
   };
 }
 
-function isManager(id) {
+function isManager(id) { // confere se id é manager
   // seu código aqui
   const manager = [];
-  employees.forEach((person) => manager.push(...person.managers));
-  return manager.some((people) => people === id);
+  employees.forEach((person) => manager.push(...person.managers)); // colocar no array manager o person.managers para cada pessoa do array employees
+  return manager.some((people) => people === id); // retorna boolean
 }
 
-function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) { // define o start do parametro como array
   // seu código aqui
   return employees.push({ id, firstName, lastName, managers, responsibleFor });
 }
@@ -42,10 +42,10 @@ function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []
 function countAnimals(specie) {
   // seu código aqui
   if (arguments.length !== 0) {
-    return species.find((animal) => animal.name === specie).residents.length;
+    return species.find((animal) => animal.name === specie).residents.length; // retorna a quantidade de animais da especia passada no parametro
   }
   return species.reduce((acc, beast) => {
-    acc[beast.name] = beast.residents.length;
+    acc[beast.name] = beast.residents.length; // acc[beast.name] => Object[key] = value
     return acc;
   }, {});
 }
@@ -53,8 +53,8 @@ function countAnimals(specie) {
 function calculateEntry(entrants) {
   // seu código aqui
   if (arguments.length === 0) return 0;
-  const { Adult = 0, Senior = 0, Child = 0 } = entrants;
-  return (prices.Adult * Adult + prices.Senior * Senior + prices.Child * Child);
+  const { Adult = 0, Senior = 0, Child = 0 } = entrants; // desestrutura e atribui no parametro
+  return (prices.Adult * Adult + prices.Senior * Senior + prices.Child * Child); // matematica basica para atribuir os valores
 }
 
 function getAnimalMap(options) {
@@ -63,25 +63,25 @@ function getAnimalMap(options) {
 
 function getSchedule(dayName) {
   // seu código aqui
-  const getInfoDay = (weekDay, schedules) => {
+  const getInfoDay = (weekDay, schedules) => { // criei uma funcao com templates para utilizar
     if (schedules.open === 0) {
-      return { [weekDay]: 'CLOSED' };
+      return { [weekDay]: 'CLOSED' }; // os couchetes são necessarios para receber o parametro como nome da chave do objeto
     }
-    return { [weekDay]: `Open from ${schedules.open}am until ${schedules.close - 12}pm` };
+    return { [weekDay]: `Open from ${schedules.open}am until ${schedules.close - 12}pm` }; // template que retorna o dia e o horario especifico
   };
-  const allDays = Object.keys(hours).map((key) => getInfoDay(key, hours[key]));
-  if (arguments.length === 0) return allDays.reduce((ac, elem) => ({ ...ac, ...elem }), {});
-  return getInfoDay(dayName, hours[dayName]);
+  const allDays = Object.keys(hours).map((key) => getInfoDay(key, hours[key])); // cria uma constante que recebe um array de objetos com o template da linha 70
+  if (arguments.length === 0) return allDays.reduce((ac, elem) => ({ ...ac, ...elem }), {}); // sem argumento retorna todos os horarios
+  return getInfoDay(dayName, hours[dayName]); // o reduce captura todos os dias e horarios por meio do acumulador e o elemento
 }
-// allDays = [ {dia: {horario}}... ]
-// Object[keys] => acessa o valor da chave do objeto
+// allDays = [ {dia: horario... ]
+// Object.keys => acessa o valor da chave do objeto
 
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
-  const employ = employees.find((emp) => emp.id === id);
-  const fisrt = species.find((bixo) => bixo.id === employ.responsibleFor[0]);
-  const oldest = fisrt.residents.sort((a, b) => b.age - a.age);
-  return Object.values(oldest[0]);
+  const employ = employees.find((emp) => emp.id === id); // procura e captura o funcionario informado no parametro
+  const fisrt = species.find((bixo) => bixo.id === employ.responsibleFor[0]); // procura e captura a primeira especie do array de responsabilidades dentro do objeto funcionario
+  const oldest = fisrt.residents.sort((a, b) => b.age - a.age); // organiza na ordem decrescente pela idade para poder retornar o primeiro
+  return Object.values(oldest[0]); // retorna o primeiro, no caso o mais velho
 }
 
 function increasePrices(percentage) {
